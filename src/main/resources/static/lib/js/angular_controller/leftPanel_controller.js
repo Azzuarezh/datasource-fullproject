fa8f0ac185b918c1ebb75db51448f0e0878fa4ca
Azzuarezh <spotlight.magic@gmail.com>
2016-08-12 19:04:36 +0700

    -converting and add some library for angular need. -converting index.html to angular (not yet completed)

--- src/main/resources/static/lib/js/angular_controller/leftPanel_controller.js
+++ src/main/resources/static/lib/js/angular_controller/leftPanel_controller.js
@@ -1,7 +1,7 @@
 /**
  * this is left panel controller angular script resolve many function in left side of the view
  */
-/*app.controller('leftPanelCtrl',function($scope,$http){
+app.controller('kiri',function($scope,$http){
 	$http.get("/getListOfDatabases")
     .then(function(response) {
         //First function handles success
@@ -9,5 +9,10 @@
     }, function(response) {
         //Second function handles error
         $scope.content = "Something went wrong";
+    }).directive('leftSidebar',function(){
+    	return{
+    		restrict :'E',
+    		template:'content : {{content.DATABASE_NAME}}' 
+    	}
     });
-})*/
+})
--- target/classes/static/lib/js/app.js
+++ target/classes/static/lib/js/app.js
@@ -4,14 +4,24 @@ this is the main function of angular.
 declare the directives here
 */
 
-	//initialize the app
-var app = angular.module('dataSourceApp',['ngAnimate','ngResource','ui.router','ui.bootstrap']);
+//initialize the app and its directive library
+var app = angular.module('dataSourceApp',
+		[
+		 'ngAnimate',
+		 'ngResource',
+		 'ui.router',
+		 'ui.bootstrap',
+		 'ui.layout',
+		 'eehNavigation',
+		 'pascalprecht.translate'
+		 ]);
+
 app.controller('modalSave', function($scope, $http) {
     $http.get("getListOfDatabases")
     .then(function(response) {
         //First function handles success
         $scope.content = response.data;
-        console.log($scope.content);
+        console.log('modal save : ',$scope.content);
     }, function(response) {
         //Second function handles error
         $scope.content = "Something went wrong";
@@ -19,48 +29,3 @@ app.controller('modalSave', function($scope, $http) {
     });
 });
 
-
-$(document).ready(function(){	
-	//w2ui panel function
-	 var pstyle = 'background-color: #fffff; border: 2px solid #dfd0d0; padding: 5px;';
-	 var pstyle2 = 'background-color: #fffff; border: 2px solid #dfd0d0; padding: 1px;';
-
-	 var topPanelObject = { type: 'top',
-	                        size: 131, 
-	                        resizable: true, 
-	                        style: pstyle2, 
-	                        content: function(){
-	                            $(this).load('panel/top.html');
-	                        }
-	                    };
-	 var leftPanelObject ={ type: 'left', 
-	                        size: 200, 
-	                        resizable: true, 
-	                        style: pstyle + 'overflow-x:hidden;', 
-	                        content: function(){
-	                            return $(this).load('panel/left.tpl.html');
-	                            } 
-	                        };
-
-	var mainPanelObject = { type: 'main', 
-	                        style: pstyle, 
-	                        content: function(){
-	                             return $(this).load('panel/main.html');
-	                            }
-	                        };
-
-	var bottomPanelObject = { type: 'bottom', 
-	                          size: 50, 
-	                          resizable: true, 
-	                          style: pstyle, 
-	                          content: function(){
-	                             return $(this).load('panel/bottom.html');
-	                            }
-	                        };
-    
-    $('#layout').w2layout({
-        name: 'layout',
-        panels: [topPanelObject,leftPanelObject,mainPanelObject,bottomPanelObject]
-    });
-    //end layout     
-})
